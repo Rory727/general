c1dd83bd3d4d36769d31279e6d50c15344a978c2
reymond-Li <36027794+Rory727@users.noreply.github.com>
2020-06-30 17:36:22 +0800

    Add files via upload
    Add pojo filed convert util

---
+++ DoGenerateUtil.java
@@ -0,0 +1,38 @@
+package com.ebay.cbt.sd.hermes.stockingmodel;
+
+/**  
+ * @author ruirli
+ */
+public class DoGenerateUtil {
+	
+	/**
+	 * 将下划线后面的一个字母转换为大写, 并删除下划线
+	 * 
+	 * trans_totel_num  change to transTotelNum
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		String changeToDoFileds = changeToDoFileds("trans_totel_num");
+		System.out.println(changeToDoFileds);
+		
+	}
+
+	
+	private static String changeToDoFileds(String value){
+		String name = value.toLowerCase();
+		char[] charArray = name.toCharArray();
+		for (int i = 0; i < charArray.length; i++) {
+			if ("_".equals(String.valueOf(charArray[i]))) {
+				if (name.charAt(i + 1) >='a' && name.charAt(i + 1) <='z') {
+					charArray[i+1] = (char) (charArray[i+1] -32);
+				}
+			}
+		}
+		String valueOf = String.valueOf(charArray);
+		valueOf = valueOf.replaceAll("_", "");
+		return valueOf;
+	}
+	
+}
